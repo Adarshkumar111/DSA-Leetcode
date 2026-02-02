d8cf8e10cc582db7b14f86690f12b01a41268641
Adarsh Bishwas <149517520+Adarshkumar111@users.noreply.github.com>
2026-02-02 15:38:06 +0530

    Time: 0 ms (100%), Space: 24.3 MB (8.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,6 +4,8 @@ public:
         int n=arr.size();
         vector<int>temp(n);
         int k=0;
+
+        // store non zero
         for(int i=0; i<n; i++){
             if(arr[i]!=0){
                 temp[k]=arr[i];
@@ -11,9 +13,12 @@ public:
             }
         }
 
+        // copy non zero in arrr
         for(int i=0; i<k; i++){
             arr[i]=temp[i];
         }
+
+        //  add 0 in last
         for(int i=k; i<n;i++){
             arr[i]=0;
         }
