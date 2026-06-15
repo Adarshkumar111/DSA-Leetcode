5c39aecbc36a48e7162cf3399c9010e7ecd0bb9b
Adarsh Bishwas <149517520+Adarshkumar111@users.noreply.github.com>
2026-06-15 15:08:07 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.96%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,10 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size()) return false;
 
-        for(int i=0; i<s.size(); i++){
-            s=s.substr(1)+s[0];
-            if(s==goal) return true;
-        }
+        return (s+s).find(goal)!=string::npos;
     return false;
     }
 };
