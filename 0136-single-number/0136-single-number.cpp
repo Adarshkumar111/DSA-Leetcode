4941a7c4e2f5e75df5cb7d20ab6743e39b0b4c24
Adarsh Bishwas <149517520+Adarshkumar111@users.noreply.github.com>
2026-06-22 11:03:37 +0530

    Time: 355 ms (5.01%), Space: 20.8 MB (23.68%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
        
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
         for(int i=0; i<n; i++){
              int cnt=0;
