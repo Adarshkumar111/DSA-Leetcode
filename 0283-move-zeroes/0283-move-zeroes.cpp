b8c2f8ba6231f845a87e8591a8248dc661fb78f8
Adarsh Bishwas <149517520+Adarshkumar111@users.noreply.github.com>
2026-07-07 11:31:23 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& arr) {
-        int n=arr.size();
-        vector<int>temp(n);
-        int k=0;
-
-        // store non zero
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int j=0;
         for(int i=0; i<n; i++){
-            if(arr[i]!=0){
-                temp[k]=arr[i];
-                k++;
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
-
-        // copy non zero in arrr
-        for(int i=0; i<k; i++){
-            arr[i]=temp[i];
-        }
-
-        //  add 0 in last
-        for(int i=k; i<n;i++){
-            arr[i]=0;
-        }
+        return ;
     }
 };
