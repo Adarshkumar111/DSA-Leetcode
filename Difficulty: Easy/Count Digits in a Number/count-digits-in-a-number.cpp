77abcea6a69d10645c2412dca1bac09ae8b3af60
Adarsh Bishwas <149517520+Adarshkumar111@users.noreply.github.com>
2026-07-23 14:51:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits in a Number/count-digits-in-a-number.cpp
+++ Difficulty: Easy/Count Digits in a Number/count-digits-in-a-number.cpp
@@ -2,13 +2,16 @@ class Solution {
   public:
     int countDigits(int n) {
         // Code here
-        int cnt=0;
+        // int cnt=0;
         
-        while(n>0){
-            n=n/10;
-            cnt++;
+        // while(n>0){
+        //     n=n/10;
+        //     cnt++;
             
-        }
-        return cnt;
+        // }
+        // return cnt;
+        
+        if(n<10) return 1;
+        return 1+countDigits(n/10);
     }
 };
