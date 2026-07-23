1d40d941baf8259f42d7497bc6ff76cc625aefca
Adarsh Bishwas <149517520+Adarshkumar111@users.noreply.github.com>
2026-07-23 12:03:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Power Using Recursion/power-using-recursion.cpp
+++ Difficulty: Basic/Power Using Recursion/power-using-recursion.cpp
@@ -2,7 +2,11 @@ class Solution {
   public:
     int recursivePower(int n, int p) {
         // code here
+        // if(p==0)return 0;
+        // if(p==1) return n;
+        // return n*pow(n,p-1);
         if(n==0) return 0;
+        
         return pow(n,p);
     }
 };
